3ede7001fd2daac1e8bb9da60948eb14f742f024
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2021-05-03 17:03:48 +0100

    Spring framework, servicing web content

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,9 +1,12 @@
 package com.example.servingwebcontent;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.xml.soap.SAAJResult;
 
 @Controller
 public class GreetingController {
@@ -13,5 +16,40 @@ public class GreetingController {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
+
+	@GetMapping("/panos")
+	public String demo(@RequestParam(name = "name",  required=false, defaultValue="World") String name,
+					   @RequestParam(name = "weather",  required=false, defaultValue="Sunny") String weather,
+					   Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("weather", weather);
+		String emotion = ":)";
+		if (!weather.equalsIgnoreCase("Sunny")) {
+			emotion = ":(";
+		}
+		model.addAttribute("emotion", emotion);
+		return "greeting";
+	}
+
+	@GetMapping("/play")
+	public String demo2(@RequestParam(name = "name",  required=false, defaultValue="One") String name,
+					   @RequestParam(name = "weather",  required=false, defaultValue="Sunny") String weather,
+					   Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("weather", weather);
+		String emotion = ":)";
+		if (!weather.equalsIgnoreCase("Sunny")) {
+			emotion = ":(";
+		}
+		model.addAttribute("emotion", emotion);
+		return "greeting";
+	}
+
+	@GetMapping("/party")
+	public String party(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "party";
+	}
+
 
 }
--- complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -10,4 +10,5 @@ public class ServingWebContentApplication {
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
 
+
 }
